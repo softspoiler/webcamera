cc70af0fbd2ab3383a474a571dfaed22c0ae260d
Rostyslav <petrishin.rostislav@gmail.com>
2018-01-30 12:02:19 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebCamComponent } from 'ack-angular-webcam';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  preview;
+  public webcam: WebCamComponent;
+  public webcamOptions: any = {
+    audio: false,
+    video: false,
+    width: 300,
+    height: 300,
+    cameraType:'back'
+  }
+  
+  onCamSuccess(e) {
+    console.log('Success', e);
+  }
+  onCamError(e) {
+    console.log('Error', e);
+  }
+
+  generateBase64() {
+    this.webcam.getBase64()
+      .then((base) => {
+        this.preview = base
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { WebCamModule } from 'ack-angular-webcam';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WebCamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
